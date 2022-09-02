04ca581c9e2a452ddd8c3f0540c8f325e6c68902
Dmitry Yavorsky <Yavorskyd@yandex.ru>
2022-09-02 13:49:51 +0300

    first commit with app icon

---
+++ Super Money/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Super Money
+//
+//  Created by Yavorsky on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after
+    }
+
+
+}
+
